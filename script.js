6de55adcc05e74278cfd89adde8461499445f8a8
binserra <118325754+binserra@users.noreply.github.com>
2022-12-06 19:44:21 -0800

    First Script

---
+++ script.js
@@ -0,0 +1,23 @@
+console.log("script file")
+//var age=10; //initialization and assignation
+// let userName="mike";
+// const taxes = 11.5;
+// const url="http://www.thisisawebpage";
+
+// console.log(age,username,url) //displaying the age, username and url
+
+// userName="Edward"; //changing the username value
+// taxes=12; //changing the taxes value
+// console.log(username,url) //displaying the username and url
+
+
+var numberChildren = "0";
+var partnersName = "Teddy";
+var geographicLocation = "San Diego";
+var jobTitle = "Medic"
+
+//concatenation use ""
+// console.log("I have " + numberChildren + " children" ,"My partners name is " + partnersName, "I live in " + geographicLocation, "My job title is " + jobTitle)
+
+//template string use ``
+document.write(` <p> You will have ${numberChildren} children in ${geographicLocation}, and married to ${partnersName} and you will be working as a ${jobTitle}</p>`)
